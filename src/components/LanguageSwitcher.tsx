3a0a929e0d0c2446b8143aa6cca596d2889d5dae
dhooa <arazza1693@gmail.com>
2025-07-05 21:40:25 +0700

    Create LanguageSwitcher.tsx
    Innit

---
+++ src/components/LanguageSwitcher.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useLocale, useTranslations } from 'next-intl';
+import { usePathname, useRouter } from 'next-intl/client';
+import { useTransition } from 'react';
+
+export const LanguageSwitcher = () => {
+  const t = useTranslations('LanguageSwitcher');
+  const locale = useLocale();
+  const router = useRouter();
+  const pathname = usePathname();
+  const [isPending, startTransition] = useTransition();
+
+  const handleLanguageChange = (nextLocale: string) => {
+    startTransition(() => {
+      router.replace(pathname, { locale: nextLocale });
+    });
+  };
+
+  return (
+    <div className="relative">
+      <select
+        defaultValue={locale}
+        onChange={(e) => handleLanguageChange(e.target.value)}
+        disabled={isPending}
+        className="cursor-pointer appearance-none rounded bg-dark-light px-3 py-1 text-white text-sm"
+      >
+        <option value="id">{t('id')}</option>
+        <option value="en">{t('en')}</option>
+      </select>
+    </div>
+  );
+};
