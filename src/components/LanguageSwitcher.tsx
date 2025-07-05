1ea752fbb065c4b4c2cf4d734116ac9c40ea3c7d
dhooa <arazza1693@gmail.com>
2025-07-05 22:44:30 +0700

    Update LanguageSwitcher.tsx

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useLocale, useTranslations } from 'next-intl';
-// PERBAIKAN UTAMA ADA DI SINI:
+// PERBAIKAN FINAL: Gunakan named import { ... } untuk semua
 import { usePathname, useRouter } from 'next-intl/navigation';
 import { useTransition } from 'react';
 
